32e5fedbf686c89fd4846413511f03574bd93fde
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-06-28 14:05:04 +0530

    insert data from database

---
+++ registration.php
@@ -0,0 +1,43 @@
+<?php
+        include './config/config.php';
+        $name = $_POST['fname'];
+        $email = $_POST['email'];
+        $number = $_POST['number'];
+        $password = $_POST['password'];
+        $dob = $_POST['dob'];
+        $address = $_POST['address'];
+        $gender = $_POST['gender'];
+        $query = "SELECT * FROM `student` WHERE `email` = '$email'";
+        $result = mysqli_query($myConnection,$query);
+        if (mysqli_num_rows($result) > 0) {
+                    // $message = "Email ID Already Exists !!";
+                    echo "<script>alert('Email ID Already Exists !!')</script>)";
+                    echo "<script>location.href='signup.html'</script>";
+                }
+                else{
+                    // echo "Data not found";
+                    $insert = "INSERT INTO `student`(`fname`,`email`,`number`,`password`,`dob`,`address`,`gender`) 
+                    VALUES('$name','$email','$number','$password','$dob','$address','$gender')";
+                    
+                    if(mysqli_query($myConnection,$insert)){
+                        echo "<script>alert('Data inserted successfully !!')</script>";
+                        // $message = "Data Inserted";
+                        echo "<script>location.href='login.html'</script>";
+                    
+                    }
+                    else{
+                    echo "Data Not Inserted";
+                    }
+
+                }
+
+      
+         
+        
+    
+
+
+
+      
+
+?>
