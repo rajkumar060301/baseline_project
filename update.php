d97d213539c03da1f9d4d500a9b156916e7cded4
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-06-30 11:40:24 +0530

    work done updation

---
+++ update.php
@@ -0,0 +1,19 @@
+<?php
+include './config/config.php';
+
+$id = $_POST["updateid"];
+$name = $_POST['fname'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+$password = $_POST['password'];
+$dob = $_POST['dob'];
+$address = $_POST['address'];
+$gender = $_POST['gender'];
+
+$query = "UPDATE `student` SET `fname`='$name',`email`='$email',`number`='$number',`password`='$password',`dob`='$dob', `address` = '$address', `gender` = '$gender' WHERE id = {$id}";
+
+mysqli_query($myConnection,$query);
+echo "Data Updated SuccessFully";
+
+
+?>
