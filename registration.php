7398de1ec75d59cdcdbf2617e15b6bdfb3718fe0
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-06-30 11:38:59 +0530

    work on insert and set gender

--- registration.php
+++ registration.php
@@ -1,5 +1,6 @@
 <?php
         include './config/config.php';
+        if($_POST["action"] == "registration"){
         $name = $_POST['fname'];
         $email = $_POST['email'];
         $number = $_POST['number'];
@@ -10,34 +11,24 @@
         $query = "SELECT * FROM `student` WHERE `email` = '$email'";
         $result = mysqli_query($myConnection,$query);
         if (mysqli_num_rows($result) > 0) {
-                    // $message = "Email ID Already Exists !!";
-                    echo "<script>alert('Email ID Already Exists !!')</script>)";
-                    echo "<script>location.href='signup.php'</script>";
+                    echo "<script>alert('Email ID Already Exists !!')</script>";
                 }
                 else{
-                    // echo "Data not found";
                     $insert = "INSERT INTO `student`(`fname`,`email`,`number`,`password`,`dob`,`address`,`gender`) 
                     VALUES('$name','$email','$number','$password','$dob','$address','$gender')";
                     
                     if(mysqli_query($myConnection,$insert)){
                         echo "<script>alert('Data inserted successfully !!')</script>";
-                        // $message = "Data Inserted";
-                        echo "<script>location.href='login.html'</script>";
                     
                     }
                     else{
-                    echo "Data Not Inserted";
+                    echo "<script>alert('Data noy inserted successfully !!')</script>";
+
                     }
 
                 }
 
-      
+            }
          
         
-    
-
-
-
-      
-
 ?>
