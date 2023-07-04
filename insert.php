532926da9b174340d68e0b83e8ebc1609d676b11
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-04 15:11:34 +0530

    changes inserted file

--- insert.php
+++ insert.php
@@ -9,8 +9,8 @@ a:hover{text-decoration:none; color:white}
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="styleseet" type="text/css" href="./css/css/bootstrap.min.css"> 
 <script>
 
 $(document).ready(function () {
@@ -78,8 +78,6 @@ if($_POST["action"] == "insert"){
 
             echo "</tbody>";
             echo "</table>";
-                                        
-
 
                         }
 
