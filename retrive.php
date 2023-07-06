38f47d3a5f990d55d7430afa9487daeb97159e09
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-06 13:49:36 +0530

    data retrive page

--- retrive.php
+++ retrive.php
@@ -22,11 +22,116 @@ $(document).ready(function () {
 
 </head>
 <body>
+  
+
+<table id="example" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Number</th>
+            <th>Password</th>
+            <th>DOB</th>
+            <th>Address</th>
+            <th>Gender</th>
+            <th>Delete Data</th>
+            <th>Update Data</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php
+include './config/config.php';
+
+$query = "select * from student";
+$result = mysqli_query($myConnection,$query);
+		if(mysqli_num_rows($result)>0){
+            // $number = 1;
+
+			while($row = mysqli_fetch_assoc($result)){
+				?>
+			
+	
+        <tr>
+            <td><?php echo $row["id"];?></td>
+            <td><?php echo $row["fname"];?></td>
+            <td><?php echo $row["email"];?></td>
+            <td><?php echo $row["number"];?></td>
+            <td><?php echo $row["password"];?></td>
+            <td><?php echo $row["dob"];?></td>
+            <td><?php echo $row["address"];?></td>
+            <td><?php echo $row["gender"];?></td>
+            <td ><button id="<?php echo $row['id']; ?>" class="btn btn-danger delbutton">Delete</button></td>
+            <!-- <td><button type="button" class="btn btn-danger" ><a href="register.php?id=<?php  echo $row["id"];?>&type=delete">Delete</a></button></td> -->
+            <td><button id="update" type="button" class="btn btn-success" onclick="getUserDetails(<?php echo $row['id']; ?>)" data-toggle="modal" data-target="#myModal">Update</button></td>
+            <!-- <td><button type="button" class="btn btn-success"><a href="register.php?id=<?php  echo $row["id"];?>&type=update&update=updateid">Update</a></button></td> -->
+
+            <?php
+     echo   "</tr>";
+			}
+    echo "</tbody>";
+ echo "</table>";
+//  $number++;
+		}
+		?>
+        <script type="text/javascript" >
+
+        $(function() {
+
+            $(".delbutton").click(function() {
+                var del_id = $(this).attr("id");
+                var info = 'id=' + del_id;
+                    $.ajax({
+                        type : "POST",
+                        url : "delete.php", //URL to the delete php script
+                        data : info,
+                        success : function() {
+                            location.reload();
+                        }
+                    });
+                   
+                return false;
+            });
+        });
+        
+ </script>
+
+<script>
+    function getUserDetails(data) {
+        // console.log(data);
+
+
+
+        // document.getElementById('hide.btn').value = data;
+        // document.cookie = "id=" + data;
+        var myValue = data; // The JavaScript value you want to pass to PHP
+
+        // var xhttp = new XMLHttpRequest();
+        // xhttp.onreadystatechange = function() {
+        // if (this.readyState == 4 && this.status == 200) {
+        //     console.log("Value stored in PHP variable successfully!");
+        // }
+        // };
+        // xhttp.open("POST", "register.php", true);
+        // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        // xhttp.send("myValue=" + encodeURIComponent(myValue));
+        // window.location.href = "register.php?myValue=" + encodeURIComponent(myValue);
+
+
+
+    }
+</script>
+
 <?php 
-    include './config/config.php';
-    // $id =  '<script>document.write(userId)</script>';
-    if(isset($row['id'])){
-    // $id = $_GET['id'];
+    // echo $row['id'];
+    // $id = 236;
+    // $myValue = $_POST['myValue']; // Retrieve the value from the AJAX request
+
+    // Store the value in a PHP variable or perform any other desired operations
+    $id = $_COOKIE['id']; // Retrieve the
+    if(isset($id)){
+    $id = $id;
     // $type = 'updateid';
 
     $query = "select * from `student` where `id` = {$id}";
@@ -47,7 +152,7 @@ $(document).ready(function () {
     }
     }
 
-if(!isset($row['id'])){
+if(!isset($id)){
 $id = ""; $name=""; $email=""; $number=""; $password=""; $date=""; $address =""; $gender="";
 
 }
@@ -93,102 +198,29 @@ $id = ""; $name=""; $email=""; $number=""; $password=""; $date=""; $address ="";
     <br>
             
     <div class="form-group">
-        <button type="button" id="update" class="btn btn-primary btn-block" onclick="updateUserDetail()"> Update</button>
+        <button type="button" id="update"  class="btn btn-primary btn-block" onclick="updateUserDetail()"> Update</button>
     </div>   
     <br>
     <!-- <div class="text-center" style="color: black;">Already have an account? <a href="login.html" style="color:blue">Login here</a></div> -->
- </form>
-        
+<!-- <input id="hide.btn" type="hidden" name="btn_value" value="" />   -->
+
+</form>
+
+
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <input type="hidden" name='' id="hidden_user">
+        
+        <button type="button" class="btn btn-default" data-dismiss="modal"  >Close</button>
       </div>
+      <!-- <?php echo $id; ?> -->
+
+      <?php 
+        echo $id; ?>
     </div>
+    <div id="show_id" ></div>
+
 
   </div>
 </div>
-<table id="example" class="display" style="width:100%">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Number</th>
-            <th>Password</th>
-            <th>DOB</th>
-            <th>Address</th>
-            <th>Gender</th>
-            <th>Delete Data</th>
-            <th>Update Data</th>
-        </tr>
-    </thead>
-    <tbody>
-
-<?php
-// include './config/config.php';
-
-$query = "select * from student";
-$result = mysqli_query($myConnection,$query);
-		if(mysqli_num_rows($result)>0){
-            // $number = 1;
-
-			while($row = mysqli_fetch_assoc($result)){
-				?>
-			
-	
-        <tr>
-            <td><?php echo $row["id"];?></td>
-            <td><?php echo $row["fname"];?></td>
-            <td><?php echo $row["email"];?></td>
-            <td><?php echo $row["number"];?></td>
-            <td><?php echo $row["password"];?></td>
-            <td><?php echo $row["dob"];?></td>
-            <td><?php echo $row["address"];?></td>
-            <td><?php echo $row["gender"];?></td>
-            <td ><button id="<?php echo $row['id']; ?>" class="btn btn-danger delbutton">Delete</button></td>
-            <!-- <td><button type="button" class="btn btn-danger" ><a href="register.php?id=<?php  echo $row["id"];?>&type=delete">Delete</a></button></td> -->
-            <td><button type="button" class="btn btn-success" onclick="getUserDetails(<?php echo $row['id']; ?>)" data-toggle="modal" data-target="#myModal">Update</button></td>
-            <!-- <td><button type="button" class="btn btn-success"><a href="register.php?id=<?php  echo $row["id"];?>&type=update&update=updateid">Update</a></button></td> -->
-
-            <?php
-     echo   "</tr>";
-			}
-    echo "</tbody>";
- echo "</table>";
-//  $number++;
-		}
-		?>
-        <script type="text/javascript" >
-        $(function() {
-
-            $(".delbutton").click(function() {
-                var del_id = $(this).attr("id");
-                var info = 'id=' + del_id;
-                    $.ajax({
-                        type : "POST",
-                        url : "delete.php", //URL to the delete php script
-                        data : info,
-                        success : function() {
-                            location.reload();
-                        }
-                    });
-                    // $(this).parents(".record").animate("fast").animate({
-                    //     opacity : "hide"
-                    // }, "slow");
-                
-                return false;
-            });
-        });
- </script>
-
-<!-- <script>
-    $(document).ready(function(){
-        $("#update").click(function(){
-
-});
-});
-</script> -->
-
 </body>
 </html>
